eb9cdaf9f960381b7e85e1a1e350332bd04d2c5d
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-11-11 17:11:29 +0700

    title error

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.retrofit.databinding.ActivityMainBinding
-import com.example.retrofit.model.Users
+import com.example.retrofit.model.Hero
 import com.example.retrofit.network.ApiClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,19 +21,19 @@ class MainActivity : AppCompatActivity() {
         val client = ApiClient.getInstance()
         val response = client.getAllUsers()
         val userList = ArrayList<String>()
-        response.enqueue(object : Callback<Users> {
-            override fun onResponse(call: Call<Users>, response: Response<Users>) {
+        response.enqueue(object : Callback<Hero> {
+            override fun onResponse(call: Call<Hero>, response: Response<Hero>) {
                 for (i in response.body()!!.data) {
-                    userList.add(i.employeeName)
+                    userList.add(i.title)
                 }
                 val listAdapter = ArrayAdapter(
                     this@MainActivity,
                     R.layout.simple_list_item_1,
                     userList
                 )
-                binding.lvNama.adapter = listAdapter
+                binding.rvHero.adapter = listAdapter
             }
-            override fun onFailure(call: Call<Users>, t: Throwable) {
+            override fun onFailure(call: Call<Hero>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "Koneksi error",
                     Toast.LENGTH_LONG).show()
             }
